9d011b1fb4e3853071b1eddd003555e324f3a3d6
Pierre-Jean Descarpentries <pierre-jean.descarpentries@epitech.eu>
2020-05-01 14:04:38 -0400

    [~] IA for skeleton

--- lib/my/lld/lld_free_r.c
+++ lib/my/lld/lld_free_r.c
@@ -12,12 +12,8 @@
 void lld_free_r(lld_t *lld)
 {
     lld_t *mov = lld->next;
-    
+
+    while (lld->data)
+        free(lld_pop(lld, 0));
     free(lld);
-    if (mov){
-        for (; mov->next; mov = mov->next)
-            free(mov->prev->data), free(mov->prev);
-        free(mov->data);
-    }
-    free(mov);
 }
