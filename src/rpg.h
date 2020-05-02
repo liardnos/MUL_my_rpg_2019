e3da90b096b713e708106ea622e862ad9f8c4d48
Loock Florent <florent.loock@epitech.eu>
2020-05-02 08:13:40 -0400

    [+] particle function

--- src/rpg.h
+++ src/rpg.h
@@ -421,4 +421,20 @@ int mob_skeleton_add(game_t *game, sfVector2f pos);
 float perlin(float x, float y);
 animator_t *init_skeleton_animator();
 
+typedef struct particle
+{
+    float x;
+    float vx;
+    float y;
+    float vy;
+    float a;
+    float va;
+    int life;
+    sfSprite *sprite;
+} particle_t;
+
+void particle_add(lld_t *p_lld, particle_t *p);
+lld_t *particle_array();
+void particles_draw(sfRenderWindow *win, lld_t *p_lld);
+
 #endif
